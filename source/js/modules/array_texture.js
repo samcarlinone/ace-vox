8124f7d5a42d0a73f144a946482f21fca0cb7f35
samcarlinone <samcarlinone@gmail.com>
2017-03-02 23:23:01 -0500

    Array texture should be working

--- source/js/modules/array_texture.js
+++ source/js/modules/array_texture.js
@@ -11,26 +11,54 @@ export class ArrayTexture {
    */
   constructor(obj) {
     var gl = AceVox.gl;
-    console.log(obj);
-    window.obj = obj;
 
-    // this.tex = gl.createTexture();
-    //
-    // gl.activeTexture(gl.TEXTURE0);
-    // gl.bindTexture(gl.TEXTURE_2D_ARRAY, this.tex);
-    // gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    // gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    // gl.texImage3D(
-    //     gl.TEXTURE_2D_ARRAY,
-    //     0,
-    //     gl.RGBA,
-    //     IMAGE_SIZE.width,
-    //     IMAGE_SIZE.height,
-    //     NUM_IMAGES,
-    //     0,
-    //     gl.RGBA,
-    //     gl.UNSIGNED_BYTE,
-    //     pixels
-    // );
+    this.tileSize = obj.imgs[0].width;
+
+    //Get smallest power of two that can hold all tiles
+    this.numTiles = 2;
+    while(this.numTiles * this.numTiles < obj.imgs.length) {
+      this.numTiles *= 2;
+    }
+    this.totalTiles = this.numTiles * this.numTiles;
+
+    var can = document.createElement('canvas');
+    can.width = can.height = this.tileSize * this.numTiles;
+
+    var ctx = can.getContext('2d');
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, can.width, can.height);
+
+    for(var y=0; y<this.numTiles; y++) {
+      for(var x=0; x<this.numTiles; x++) {
+        if((x + y*this.numTiles) < obj.imgs.length) {
+          ctx.drawImage(obj.imgs[x + y*this.numTiles], x*this.tileSize, y*this.tileSize);
+        } else {
+          break;
+        }
+      }
+    }
+
+    var pixels = ctx.getImageData(0, 0, can.width, can.height);
+    pixels = new Uint8Array(pixels.data.buffer);
+    console.log(pixels, this.tileSize, this.totalTiles);
+
+    this.tex = gl.createTexture();
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D_ARRAY, this.tex);
+    gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texImage3D(
+        gl.TEXTURE_2D_ARRAY,
+        0,
+        gl.RGBA,
+        this.tileSize,
+        this.tileSize,
+        this.totalTiles,
+        0,
+        gl.RGBA,
+        gl.UNSIGNED_BYTE,
+        pixels
+    );
   }
 }
