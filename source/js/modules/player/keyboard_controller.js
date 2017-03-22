f2d74ba5a8771251454aa00a5206583adacaf13d
samcarlinone <samcarlinone@gmail.com>
2017-03-22 12:28:04 -0400

    Add mouse button support to keyboard controller

--- source/js/modules/player/keyboard_controller.js
+++ source/js/modules/player/keyboard_controller.js
@@ -1,13 +1,19 @@
 class KeyboardController {
+  static get M1() { return 0; }
+  static get M2() { return 2; }
+
   constructor() {
     //Keyboard stuff
-    this.state = {vx: [0], vy: [0], vz: [0]};
-    this.key = [{code: [65], val: -1, prop: "vx"},
-                {code: [69], val: -1, prop: "vy"},
-                {code: [87], val: -1, prop: "vz"},
-                {code: [68], val:  1, prop: "vx"},
-                {code: [81], val:  1, prop: "vy"},
-                {code: [83], val:  1, prop: "vz"}];
+    this.state = {vx: [0], vy: [0], vz: [0], M1: [0]};
+    this.key = [
+      {code: [65], val: -1, prop: "vx"},
+      {code: [69], val: -1, prop: "vy"},
+      {code: [87], val: -1, prop: "vz"},
+      {code: [68], val:  1, prop: "vx"},
+      {code: [81], val:  1, prop: "vy"},
+      {code: [83], val:  1, prop: "vz"},
+      {code: [KeyboardController.M1], val: 1, prop: "M1"}
+    ];
 
     // W: 87
     // S: 83
@@ -19,6 +25,8 @@ class KeyboardController {
     //Register Events
     window.addEventListener('keydown', (e) => {this.keyDown(e)}, false);
     window.addEventListener('keyup', (e) => {this.keyUp(e)}, false);
+    window.addEventListener('mousedown', (e) => {this.keyDown(e)}, false);
+    window.addEventListener('mouseup', (e) => {this.keyUp(e)}, false);
   }
 
   getState(name) {
@@ -26,6 +34,9 @@ class KeyboardController {
   }
 
   keyDown(e) {
+    if(e.keyCode === undefined)
+      e.keyCode = e.button;
+
     //console.log(e.keyCode);
 
     for(var i=0; i<this.key.length; i++) {
@@ -39,6 +50,9 @@ class KeyboardController {
   }
 
   keyUp(e) {
+    if(e.keyCode === undefined)
+      e.keyCode = e.button;
+
     for(var i=0; i<this.key.length; i++) {
       if(this.key[i].code.indexOf(e.keyCode) != -1) {
         var index = this.state[this.key[i].prop].indexOf(this.key[i].val);
