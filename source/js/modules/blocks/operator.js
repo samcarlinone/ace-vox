a285a619067400bfb6b3fb97b049618f0746f69e
samcarlinone <samcarlinone@gmail.com>
2017-03-23 13:26:23 -0400

    Fixed normal border fill

--- source/js/modules/blocks/operator.js
+++ source/js/modules/blocks/operator.js
@@ -4,7 +4,7 @@ import {Chunk} from './chunk.js';
 function operate(op_code, op_data, chunk) {
   switch (op_code) {
     case ChunkBuilder.UPDATE_B:
-      var other_chunk = chunk.world.chunkStore.getObj([chunk.position[0]+op_data[0]*64, chunk.position[1]+op_data[1]*64, chunk.position[2]+op_data[2]*64]);
+      var other_chunk = chunk.world.chunkStore.getObj([chunk.position[0]-op_data[0]*64, chunk.position[1]-op_data[1]*64, chunk.position[2]-op_data[2]*64]);
 
       if(other_chunk == -1)
         return true;
@@ -14,7 +14,7 @@ function operate(op_code, op_data, chunk) {
 
       if(op_data[0] !== 0) {
         var dir_index = getDirectionIndex(op_data[0], op_data[1], op_data[2]);
-        var offset = (op_data[0] > 0) ? 63 : 0;
+        var offset = (op_data[0] < 0) ? 63 : 0;
         for(var y=0; y<64; y++) {
           for(var z=0; z<64; z++) {
             other_chunk.bData[y+z*Chunk.SIZE_1 + Chunk.SIZE_2*dir_index] = chunk.data[offset + z*Chunk.SIZE_1 + y*Chunk.SIZE_2];
@@ -23,7 +23,7 @@ function operate(op_code, op_data, chunk) {
       }
       if(op_data[1] !== 0) {
         var dir_index = getDirectionIndex(op_data[0], op_data[1], op_data[2]);
-        var offset = (op_data[1] > 0) ? 63*Chunk.SIZE_2 : 0;
+        var offset = (op_data[1] < 0) ? 63*Chunk.SIZE_2 : 0;
         for(var x=0; x<64; x++) {
           for(var z=0; z<64; z++) {
             other_chunk.bData[x+z*Chunk.SIZE_1 + Chunk.SIZE_2*dir_index] = chunk.data[x + z*Chunk.SIZE_1 + offset];
@@ -32,7 +32,7 @@ function operate(op_code, op_data, chunk) {
       }
       if(op_data[2] !== 0) {
         var dir_index = getDirectionIndex(op_data[0], op_data[1], op_data[2]);
-        var offset = (op_data[2] > 0) ? 63*Chunk.SIZE_1 : 0;
+        var offset = (op_data[2] < 0) ? 63*Chunk.SIZE_1 : 0;
         for(var x=0; x<64; x++) {
           for(var y=0; y<64; y++) {
             other_chunk.bData[x+y*Chunk.SIZE_1 + Chunk.SIZE_2*dir_index] = chunk.data[x + offset + y*Chunk.SIZE_2];
